7212af76fb8ce57c6b8a44bc71809e43a1d2a44a
GURUAKASHSM <109951850+GURUAKASHSM@users.noreply.github.com>
2023-10-25 10:59:21 +0530

    Update config.go

--- config/config.go
+++ config/config.go
@@ -2,15 +2,14 @@ package config
 
 import (
 	"context"
-	"fmt"
 	"ecommerce/constants"
+	"fmt"
 	"log"
+
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-
 
 var Customer_Collection *mongo.Collection
 var Cart_Collection *mongo.Collection
@@ -18,6 +17,8 @@ var Seller_Collection *mongo.Collection
 var Inventory_Collection *mongo.Collection
 var Feedback_Collection *mongo.Collection
 var Buynow_Collection *mongo.Collection
+
+
 
 func init() {
 	clientoption := options.Client().ApplyURI(constants.Connectstring)
@@ -28,11 +29,11 @@ func init() {
 	}
 	fmt.Println("MongoDb sucessfully connected")
 	Customer_Collection = client.Database(constants.DB_Name).Collection(constants.Customer_collection)
-    Cart_Collection = client.Database(constants.DB_Name).Collection(constants.Cart_collection)
+	Cart_Collection = client.Database(constants.DB_Name).Collection(constants.Cart_collection)
 	Seller_Collection = client.Database(constants.DB_Name).Collection(constants.Seller_Collection)
 	Inventory_Collection = client.Database(constants.DB_Name).Collection(constants.Inventory_Collection)
 	Feedback_Collection = client.Database(constants.DB_Name).Collection(constants.Feedback_Collection)
 	Buynow_Collection = client.Database(constants.DB_Name).Collection(constants.BuyItems_Collection)
-	
+
 	fmt.Println("All Collection Connected")
 }
